e58acdf29ea5024b3236f1b31cb59d4305ea7b44
SILAS-JOB <tiilas777@gmail.com>
2026-01-18 20:14:25 -0300

    example lft

--- intro-book/borrow-checker/src/main.rs
+++ intro-book/borrow-checker/src/main.rs
@@ -1,9 +1,15 @@
-fn main() {
-    let r;
-    {
-        let x = 5;
-
-        let r = &x;
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
-    println!("{x} , {r}");
+}
+
+fn main() {
+    let string1 = String::from("pilas");
+    let string2 = "wilass";
+
+    let result = longest(&string1, &string2);
+    println!("{result}");
 }

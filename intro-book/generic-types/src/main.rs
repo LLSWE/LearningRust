60a6084b23168ea6e9c67fda63ecefbf6faa81f3
SILAS-JOB <tiilas777@gmail.com>
2026-01-18 11:19:03 -0300

    example start

---
+++ intro-book/generic-types/src/main.rs
@@ -0,0 +1,17 @@
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![10, 20, 30, 40, 50];
+    let result = largest(&number_list);
+    println!("{result}");
+}
